b589d3e356a6726dc30d1009fbc3fd71c4f5a110
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-09-21 22:39:08 +0500

    fixed a typo

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -18,7 +18,7 @@ const Header = () => {
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <Link className="navbar-brand navbar-text" to='/'>{t('description.head')}</Link>
+                    <Link className="navbar-brand navbar-text" to='/'>{t('description.Nav')}</Link>
                     <button className="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <i className="fas fa-bars" />
                     </button>
