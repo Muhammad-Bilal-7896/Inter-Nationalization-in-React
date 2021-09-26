932390947040be99a5ddff942a758d1590d5e6c6
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-09-27 00:42:30 +0500

    feat

--- src/Components/FirebaseData/index.jsx
+++ src/Components/FirebaseData/index.jsx
@@ -19,58 +19,59 @@ const lngs = {
     ar: { nativeName: 'Arabic' }
 };
 
+const convertArrayToObject = (array, key) => {
+    const initialValue = {};
+    return array.reduce((obj, item) => {
+        return {
+            ...obj,
+            [item.translation.description.short_name]: item,
+        };
+    }, initialValue);
+};
+
 const FirebaseData = () => {
-
-    const [data, setData] = useState({});
-
-    const convertArrayToObject = (array, key) => {
-        const initialValue = {};
-        return array.reduce((obj, item) => {
-            return {
-                ...obj,
-                [item.translation.description.short_name]: item,
-            };
-        }, initialValue);
-    };
-
-    useEffect(() => {
-        //////////////////////////////////////////////////////////////////////////////////////////Retreiving firebase data from web
-        // Now retreiving the data
-        //////////////////////////////Here all data retreiving is working////////////////////////////
-        const db = firebase.firestore();
-        //Data Retreiving for Auth
-        db.collection(`Languages`)
-            .get()
-            .then(snapshot => {
-                let data = [];
-                snapshot.forEach(element => {
-                    data.push(Object.assign({
-                        "id": element.id,
-                        "translation": element.translation
-                    }, element.data()))
-                })
-
-                const language_array = [];
-
-                for (let i = 0; i < data.length; i++) {
-                    //console.log(data[i].translation.description.short_name);
-                    let language_label = data[i].translation.description.short_name;
-                    language_array.push(language_label);
-                }
-                console.log(language_array);
-                //                console.log(`data for current selected user `, convertArrayToObject(data, 1));
-                setData(convertArrayToObject(data, 1));
+    const db = firebase.firestore();
+    var actual;
+    //Data Retreiving for Auth
+    var aaa={};
+    db.collection(`Languages`)
+        .get()
+        .then(snapshot => {
+            let data = [];
+            snapshot.forEach(element => {
+                data.push(Object.assign({
+                    "id": element.id,
+                    "translation": element.translation
+                }, element.data()))
             })
-        //////////////////////////////////////////////////////////////////////////////////////////Retreiving firebase data from web
-    })
 
+            const language_array = [];
 
-    return (
-        (data != {}) ? (
-            data
-        ) : (
-            <></>
-        )
-    )
+            for (let i = 0; i < data.length; i++) {
+                //console.log(data[i].translation.description.short_name);
+                let language_label = data[i].translation.description.short_name;
+                language_array.push(language_label);
+            }
+            console.log(language_array);
+            //                console.log(`data for current selected user `, convertArrayToObject(data, 1));
+            //setData(convertArrayToObject(data, 1));
+            actual = convertArrayToObject(data, 1);
+            console.log("Actual is equal to : ", actual);
+            aaa = getData(actual);
+        })
+    //////////////////////////////////////////////////////////////////////////////////////////Retreiving firebase data from web
+    return aaa;
 }
-export default FirebaseData;
+
+FirebaseData();
+
+const getData = (data) => {
+    console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAA BBBBBBBBBBBBBBBBBBB CCCCCCCCCCCC",data)
+    return data;
+}
+
+export {
+    FirebaseData,
+    getData
+}
+
