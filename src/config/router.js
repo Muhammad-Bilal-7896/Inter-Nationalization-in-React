51eaf7768f82550aff3dbd570f298a2a55bec033
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-10-20 18:16:39 +0500

    feat

--- src/config/router.js
+++ src/config/router.js
@@ -19,7 +19,12 @@ function AppRouter() {
     <div>
       <Router>
         <Route exact path={"/"} component={Home} />
-        <Route exact path={baseRouteUrl + "/"} component={Home} />
+        {(baseRouteUrl === "/") ? (
+          <Route exact path={baseRouteUrl + "/"} component={Home} />
+        ) : (
+          <></>
+        )}
+
         <Route exact path={"/about"} component={About} />
         <Route path={`${baseRouteUrl}/${t('description.url')}`} component={About} />
       </Router>
